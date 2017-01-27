bcc98cdeb45b7d95880e61a644255d381957f81a
Scott Tse <Scott Tse>
2017-01-26 23:31:59 -0800

    add SQL scripts for table creation

---
+++ postgresql/sql/create-table-agency.sql
@@ -0,0 +1,5 @@
+CREATE TABLE agency (
+        Agency_ID INTEGER NOT NULL,
+        Description VARCHAR(34) NOT NULL,
+        StateCode VARCHAR(4) NOT NULL
+);
---
+++ postgresql/sql/create-table-alarmlevel.sql
@@ -0,0 +1,5 @@
+CREATE TABLE alarmlevel (
+        AlarmLevel_ID INTEGER NOT NULL,
+        Description INTEGER NOT NULL,
+        ID_911 INTEGER NOT NULL
+);
